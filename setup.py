70bdeff702a20826381d9b4b6f18e88c85f77ceb
adrianliaw <adrianliaw2000@gmail.com>
2016-06-14 21:49:27 +0800

    Add package dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(
     name="fuckbrain",
     version="0.1",
     packages=["fuckbrain"],
+    install_requires=[
+        "jupyter_client",
+        "ipykernel",
+        "clime",
+    ],
     package_data={
         "fuckbrain": ["*.pkl"]
-    }
+    },
 )
